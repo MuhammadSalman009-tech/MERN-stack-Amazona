1e8f5195f4f8340162e4c0a00fd0c0e8e1e20725
MuhammadSalman009-tech <salmansumra009@gmail.com>
2021-03-31 21:15:05 +0500

    react and material-ui initial setup

---
+++ src/Products.js
@@ -0,0 +1,69 @@
+const products=[
+    {
+        _id:1,
+        name:"Nike Shirt",
+        description:"This is nike shirt description.",
+        image:"/assets/images/uthappizza.png",
+        price:123,
+        rating:4.5,
+        numReviews:10,
+        brand:"Nike",
+        category:"shirt"
+    },
+    {
+        _id:2,
+        name:"Nike shoes",
+        description:"This is nike shoes description.",
+        image:"/assets/images/uthappizza.png",
+        price:24,
+        rating:4.0,
+        numReviews:10,
+        brand:"Nike",
+        category:"shoes"
+    },
+    {
+        _id:3,
+        name:"Nike bag",
+        description:"This is nike bag description.",
+        image:"/assets/images/uthappizza.png",
+        price:140,
+        rating:4.5,
+        numReviews:13,
+        brand:"Nike",
+        category:"bag"
+    },
+    {
+        _id:4,
+        name:"Puma shirt",
+        description:"This is puma shirt description.",
+        image:"/assets/images/uthappizza.png",
+        price:12,
+        rating:4.5,
+        numReviews:17,
+        brand:"Puma",
+        category:"shirt"
+    },
+    {
+        _id:1,
+        name:"Puma Slim Pant",
+        description:"This is puma pant description.",
+        image:"/assets/images/uthappizza.png",
+        price:110,
+        rating:4.5,
+        numReviews:13,
+        brand:"Puma",
+        category:"pant"
+    },
+    {
+        _id:6,
+        name:"Addidas Shirt",
+        description:"This is addidas shirt description.",
+        image:"/assets/images/uthappizza.png",
+        price:76,
+        rating:4.5,
+        numReviews:10,
+        brand:"Addidas",
+        category:"shirt"
+    },
+]
+export default products;
